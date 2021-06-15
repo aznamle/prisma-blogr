a3d3969f327afd6902e43993a8d190588d1b7eaa
aznamle <namle1998@hotmail.com>
2021-06-15 16:31:54 -0700

    added github Oauth signin

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,16 +1,19 @@
-import React from "react";
-import Link from "next/link";
-import { useRouter } from "next/router";
+import React from 'react'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import { signOut, useSession } from 'next-auth/client'
 
 const Header: React.FC = () => {
-  const router = useRouter();
+  const router = useRouter()
   const isActive: (pathname: string) => boolean = (pathname) =>
-    router.pathname === pathname;
+    router.pathname === pathname
+
+  const [session, loading] = useSession()
 
   let left = (
     <div className="left">
       <Link href="/">
-        <a className="bold" data-active={isActive("/")}>
+        <a className="bold" data-active={isActive('/')}>
           Feed
         </a>
       </Link>
@@ -25,7 +28,7 @@ const Header: React.FC = () => {
           display: inline-block;
         }
 
-        .left a[data-active="true"] {
+        .left a[data-active='true'] {
           color: gray;
         }
 
@@ -34,9 +37,161 @@ const Header: React.FC = () => {
         }
       `}</style>
     </div>
-  );
+  )
 
-  let right = null;
+  let right = null
+
+  if (loading) {
+    left = (
+      <div className="left">
+        <Link href="/">
+          <a className="bold" data-active={isActive('/')}>
+            Feed
+          </a>
+        </Link>
+        <style jsx>{`
+          .bold {
+            font-weight: bold;
+          }
+
+          a {
+            text-decoration: none;
+            color: #000;
+            display: inline-block;
+          }
+
+          .left a[data-active='true'] {
+            color: gray;
+          }
+
+          a + a {
+            margin-left: 1rem;
+          }
+        `}</style>
+      </div>
+    )
+    right = (
+      <div className="right">
+        <p>Validating session ...</p>
+        <style jsx>{`
+          .right {
+            margin-left: auto;
+          }
+        `}</style>
+      </div>
+    )
+  }
+
+  if (!session) {
+    right = (
+      <div className="right">
+        <Link href="/api/auth/signin">
+          <a data-active={isActive('/signup')}>Log in</a>
+        </Link>
+        <style jsx>{`
+          a {
+            text-decoration: none;
+            color: #000;
+            display: inline-block;
+          }
+
+          a + a {
+            margin-left: 1rem;
+          }
+
+          .right {
+            margin-left: auto;
+          }
+
+          .right a {
+            border: 1px solid black;
+            padding: 0.5rem 1rem;
+            border-radius: 3px;
+          }
+        `}</style>
+      </div>
+    )
+  }
+
+  if (session) {
+    left = (
+      <div className="left">
+        <Link href="/">
+          <a className="bold" data-active={isActive('/')}>
+            Feed
+          </a>
+        </Link>
+        <Link href="/drafts">
+          <a data-active={isActive('/drafts')}>My drafts</a>
+        </Link>
+        <style jsx>{`
+          .bold {
+            font-weight: bold;
+          }
+
+          a {
+            text-decoration: none;
+            color: #000;
+            display: inline-block;
+          }
+
+          .left a[data-active='true'] {
+            color: gray;
+          }
+
+          a + a {
+            margin-left: 1rem;
+          }
+        `}</style>
+      </div>
+    )
+    right = (
+      <div className="right">
+        <p>
+          {session.user.name} ({session.user.email})
+        </p>
+        <Link href="/create">
+          <button>
+            <a>New post</a>
+          </button>
+        </Link>
+        <button onClick={() => signOut()}>
+          <a>Log out</a>
+        </button>
+        <style jsx>{`
+          a {
+            text-decoration: none;
+            color: #000;
+            display: inline-block;
+          }
+
+          p {
+            display: inline-block;
+            font-size: 13px;
+            padding-right: 1rem;
+          }
+
+          a + a {
+            margin-left: 1rem;
+          }
+
+          .right {
+            margin-left: auto;
+          }
+
+          .right a {
+            border: 1px solid black;
+            padding: 0.5rem 1rem;
+            border-radius: 3px;
+          }
+
+          button {
+            border: none;
+          }
+        `}</style>
+      </div>
+    )
+  }
 
   return (
     <nav>
@@ -50,7 +205,7 @@ const Header: React.FC = () => {
         }
       `}</style>
     </nav>
-  );
-};
+  )
+}
 
-export default Header;
+export default Header
