f6f12d1b91bee5b64f3326845e8547b3a9739587
aznamle <namle1998@hotmail.com>
2021-06-16 12:08:58 -0700

    deploy fixes

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,11 +1,2 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
-declare global {
-    namespace NodeJS {
-        interface Global {
-            prisma: any;
-        }
-    }
-}
-
-export { }
